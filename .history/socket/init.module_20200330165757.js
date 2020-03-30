abf1d4e5f5611aef3747c53b5403ceac2df41022
unknown <rbii aziz>
2020-03-30 18:38:16 +0100

    30

---
+++ .history/socket/init.module_20200330165757.js
@@ -0,0 +1,58 @@
+const socketStore = require('./socket.store.js');
+let chat = []   //contient des objects { id : xx , messages : [] }
+const moment = require('moment-timezone');
+const users = []
+const usernames = []
+module.exports = (io) => {
+    io.on('connection', function (socket) {
+
+        console.log('\n connection someone connect', socket.id);
+        socket.on('user_connected', function () {
+         console.log("aziz")
+        })
+
+        socket.on('first', (messages, idchat) => {
+            let x = 0
+            for (var i = 0; i < chat.length; i++) {
+                if (chat[i].id == idchat) {
+                    x = 1
+                    break
+                }
+            }
+
+            if (x == 0) {
+                let newchat = {
+                    id: idchat,
+                    messages: messages
+                }
+               chat.push(newchat)
+            }
+        });
+
+        socket.on('msg', (username, message, id) => {
+
+            objmessage = {}
+            for (var i = 0; i < chat.length; i++) {
+                if (chat[i].id == id) {
+                    objmessage.sender = username
+                    objmessage.contenu = message
+                    chat[i].messages.push(objmessage)
+
+                    break
+                }
+            }
+            //     io.emit('msg', {message,id})
+            io.emit('get', chat)
+
+
+        });
+
+        socket.on('disconnect', () => {
+            console.log('someone disconnect');
+            socketStore.removeSocket(socket);
+        });
+    });
+    io.on('disconnect', function () {
+
+    });
+}
